eec16f3efee3e2108d702e497114e381b33db811
Ankit <ankitjhamahi.07@gmail.com>
2021-11-09 02:13:22 +0530

    random-quote-generator

---
+++ README.markdown
@@ -0,0 +1,5 @@
+# Random Quote Generator Vanilla HTML/CSS/JS Only
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/akj007ind/pen/wvqjjay](https://codepen.io/akj007ind/pen/wvqjjay).
+
+
---
+++ dist/script.js
@@ -0,0 +1,31 @@
+function getQuote() {
+ let quote="";
+return fetch("https://api.quotable.io/random")
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(data) {
+  let quoteContainer=document.getElementById("quote-container");
+  let guideText=document.getElementById("guide-text");
+  
+  let quoteText=document.getElementById("quote-text");
+  let quoteAuthor=document.getElementById("author-name");
+  //let quoteAuthorJob=document.getElementById("author-job");
+  quoteText.innerHTML=data.content;
+  quoteAuthor.innerHTML=data.author;
+  // quoteAuthorJob.innerHTML=data.tags[0];
+  quoteContainer.style.display="block";
+  guideText.style.display="none";
+}
+);
+}
+
+function copyQuote() {
+  
+  var range = document.createRange();
+                    range.selectNode(document.getElementById("quote-text"));
+                    window.getSelection().removeAllRanges();  window.getSelection().addRange(range); // to select text
+ document.execCommand("copy");
+ window.getSelection().removeAllRanges();
+ window.alert("Copied to Clipboard")
+}
